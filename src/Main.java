2527b2d2ba8079027868505e500f8049a12685e2
lSindra <lucassindra01@hotmail.com>
2017-12-03 21:57:57 -0200

    Create window function made

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,21 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("uma palavra");
+        createWindow();
     }
+
+    private static void createWindow() {
+
+        JFrame frame = new JFrame("NotePad--");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel textLabel = new JLabel("You'll be able to write down things soon enough",SwingConstants.CENTER);
+        textLabel.setPreferredSize(new Dimension(300, 100));
+        frame.getContentPane().add(textLabel, BorderLayout.CENTER);
+
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
