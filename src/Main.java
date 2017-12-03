8be06a002c76974b8fe4534305f89a6603b7a759
lSindra <lucassindra01@hotmail.com>
2017-12-03 21:47:07 -0200

    Made important imports

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Scanner;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("uma palavra");
