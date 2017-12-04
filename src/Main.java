74b2ae823a69096153af60811f7716ba07cb6ac7
lSindra <lucassindra01@hotmail.com>
2017-12-03 22:27:53 -0200

    almost done close window function

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
         JFrame frame = new JFrame("NotePad--");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        frame.add(createButton(50, 25));
+        frame.add(createButton(50, 25, "oi"));
 
         JLabel textLabel = new JLabel("You'll be able to write down things soon enough",SwingConstants.CENTER);
         textLabel.setPreferredSize(new Dimension(900, 720));
@@ -25,14 +25,6 @@ public class Main {
         frame.setVisible(true);
     }
 
-    private static JButton createButton(int width, int heigth) {
-        JButton b1 = new JButton();
-        b1.setSize(width, heigth);
-        b1.setVisible(true);
-        b1.setText("HelloWorld");
-        return b1;
-    }
-
     private static JButton createButton(int width, int heigth, String buttonText) {
         JButton b1 = new JButton();
         b1.setSize(width, heigth);
@@ -40,5 +32,15 @@ public class Main {
         b1.setText(buttonText);
         return b1;
     }
+
+    private JButton createButton(int width, int heigth, String buttonText, String function) {
+        JButton b1 = new JButton();
+        b1.setSize(width, heigth);
+        b1.setVisible(true);
+        b1.setText(buttonText);
+        b1.addActionListener(e -> this.dispose());
+        return b1;
+    }
+
 
 }
